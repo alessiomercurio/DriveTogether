831eac973b97fc909ae59bf32bab9c7a345eeda3
Alessio <alessiomercu@gmail.com>
2023-06-12 19:09:03 +0200

    Rimossa la status bar e aggiustato gli id

--- app/src/main/java/com/agmobiletech/drivetogether/MainActivity.kt
+++ app/src/main/java/com/agmobiletech/drivetogether/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.agmobiletech.drivetogether
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_login)
     }
 
 }
