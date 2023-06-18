c31b6de34506444a75e3280fff919caa11891280
gabriel <gabrielmarchione2@gmail.com>
2023-06-18 16:44:38 +0200

    creata bozza del layout dell'inserimento della macchina

---
+++ app/src/main/java/com/agmobiletech/drivetogether/InserimentoAutoActivity.kt
@@ -0,0 +1,11 @@
+package com.agmobiletech.drivetogether
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InserimentoAutoActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/agmobiletech/drivetogether/homepage/HomepageActivity.kt
+++ app/src/main/java/com/agmobiletech/drivetogether/homepage/HomepageActivity.kt
@@ -6,21 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.agmobiletech.drivetogether.BottomNavigationManager
 import com.agmobiletech.drivetogether.R
 import com.agmobiletech.drivetogether.databinding.ActivityHomepageBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomepageActivity : AppCompatActivity() {
-    lateinit var binding: ActivityHomepageBinding
-    lateinit var navigationManager: BottomNavigationManager
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivityHomepageBinding
+    private lateinit var navigationManager: BottomNavigationManager
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomepageBinding.inflate(layoutInflater)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+        val bottomNavigationView = binding.bottomNavigationBar
         bottomNavigationView.selectedItemId = R.id.homepageMenuItem
         navigationManager = BottomNavigationManager(this, bottomNavigationView)
-
     }
 }
