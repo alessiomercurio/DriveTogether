89c841d3cdcea5dc2c717978d6aec141f15832fb
Alessio <alessiomercu@gmail.com>
2023-06-24 17:40:55 +0200

    merda

--- app/src/main/java/com/agmobiletech/drivetogether/inserimentoAuto/InserimentoAutoActivity.kt
+++ app/src/main/java/com/agmobiletech/drivetogether/inserimentoAuto/InserimentoAutoActivity.kt
@@ -3,9 +3,16 @@ package com.agmobiletech.drivetogether.inserimentoAuto
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.view.Window
+import android.widget.SearchView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import com.agmobiletech.drivetogether.BottomNavigationManager
 import com.agmobiletech.drivetogether.ClientNetwork
 import com.agmobiletech.drivetogether.R
@@ -15,20 +22,151 @@ import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import java.io.File
-import java.util.Objects
+import androidx.lifecycle.lifecycleScope
+import com.mapbox.search.ApiType
+import com.mapbox.search.ResponseInfo
+import com.mapbox.search.SearchEngine
+import com.mapbox.search.SearchEngineSettings
+import com.mapbox.search.autocomplete.PlaceAutocomplete
+import com.mapbox.search.autofill.Query
+import com.mapbox.search.offline.OfflineResponseInfo
+import com.mapbox.search.offline.OfflineSearchEngine
+import com.mapbox.search.offline.OfflineSearchEngineSettings
+import com.mapbox.search.offline.OfflineSearchResult
+import com.mapbox.search.record.HistoryRecord
+import com.mapbox.search.result.SearchResult
+import com.mapbox.search.result.SearchSuggestion
+import com.mapbox.search.ui.adapter.engines.SearchEngineUiAdapter
+import com.mapbox.search.ui.view.CommonSearchViewConfiguration
+import com.mapbox.search.ui.view.DistanceUnitType
+import com.mapbox.search.ui.view.SearchMode
+import com.mapbox.search.ui.view.SearchResultsView
+import kotlinx.coroutines.launch
 
 class InserimentoAutoActivity : AppCompatActivity(){
     //inserimento spinner
     lateinit var binding : ActivityInserimentoAutoBinding
     lateinit var navigationManager: BottomNavigationManager
     lateinit var filePre : SharedPreferences
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var searchView: SearchView
+
+    private lateinit var searchResultsView: SearchResultsView
+    private lateinit var searchEngineUiAdapter: SearchEngineUiAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInserimentoAutoBinding.inflate(layoutInflater)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
+
+        // prova
+        searchResultsView = findViewById<SearchResultsView>(R.id.search_results_view)
+        // searchResultsView initialization
+
+        searchResultsView.initialize(
+            SearchResultsView.Configuration(
+                CommonSearchViewConfiguration(DistanceUnitType.IMPERIAL)
+            )
+        )
+
+        val searchEngine = SearchEngine.createSearchEngineWithBuiltInDataProviders(
+            SearchEngineSettings(getString(R.string.mapbox_access_token))
+        )
+
+        val offlineSearchEngine = OfflineSearchEngine.create(
+            OfflineSearchEngineSettings(getString(R.string.mapbox_access_token))
+        )
+
+        searchEngineUiAdapter = SearchEngineUiAdapter(
+            view = searchResultsView,
+            searchEngine = searchEngine,
+            offlineSearchEngine = offlineSearchEngine,
+        )
+
+
+        searchEngineUiAdapter.addSearchListener(object : SearchEngineUiAdapter.SearchListener {
+
+            override fun onSuggestionsShown(
+                suggestions: List<SearchSuggestion>,
+                responseInfo: ResponseInfo
+            ) {
+                // Nothing to do
+                searchResultsView.visibility = View.VISIBLE
+                System.out.println("Funziona9254")
+            }
+
+            override fun onError(e: Exception) {
+                System.out.println("Funziona9054")
+            }
+
+            override fun onFeedbackItemClick(responseInfo: ResponseInfo) {
+                System.out.println("Funziona102")
+            }
+
+            override fun onHistoryItemClick(historyRecord: HistoryRecord) {
+                System.out.println("Funziona00")
+            }
+
+            override fun onOfflineSearchResultSelected(
+                searchResult: OfflineSearchResult,
+                responseInfo: OfflineResponseInfo
+            ) {
+                System.out.println("Funziona1312")
+            }
+
+            override fun onOfflineSearchResultsShown(
+                results: List<OfflineSearchResult>,
+                responseInfo: OfflineResponseInfo
+            ) {
+                System.out.println("Funziona1")
+            }
+
+            override fun onPopulateQueryClick(
+                suggestion: SearchSuggestion,
+                responseInfo: ResponseInfo
+            ) {
+                System.out.println("Funziona4")
+            }
+
+            override fun onSearchResultSelected(
+                searchResult: SearchResult,
+                responseInfo: ResponseInfo
+            ) {
+                System.out.println("Funziona19")
+            }
+
+            override fun onSearchResultsShown(
+                suggestion: SearchSuggestion,
+                results: List<SearchResult>,
+                responseInfo: ResponseInfo
+            ) {
+                System.out.println("Funziona23")
+                searchResultsView.visibility = View.VISIBLE
+            }
+
+            override fun onSuggestionSelected(searchSuggestion: SearchSuggestion): Boolean {
+                return false
+            }
+        })
+
+        binding.posizionePlainText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val query = Query.create(p0.toString())
+                if (query != null) {
+                    searchEngineUiAdapter.search(p0.toString())
+                }
+                searchResultsView.visibility = View.VISIBLE
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
 
         //seleziono la navbar prendendo l'id
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
@@ -116,5 +254,10 @@ class InserimentoAutoActivity : AppCompatActivity(){
         val path = marca.lowercase()
         
         return "media/images/loghi/${path}.svg"
+    }
+
+    private fun closeSearchView() {
+        toolbar.collapseActionView()
+        searchView.setQuery("", false)
     }
 }
